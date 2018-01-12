6593ca150d685ab1c7df818f366ed80ad508d7de
ysoar <ytf2528621@163.com>
2018-01-12 15:05:23 +0800

    修改bug

--- src/MeituanOpenApi/Protocol/RpcClient.php
+++ src/MeituanOpenApi/Protocol/RpcClient.php
@@ -74,7 +74,6 @@ class RpcClient
 
         //签名sign
         $protocol['sign'] = $this->generateSignature($protocol);
-
         if ($method == 'get') { //get
             $result = $this->get($url, $protocol, $header);
         } else { //post
@@ -85,7 +84,6 @@ class RpcClient
         if (is_null($response)) {
             throw new Exception("invalid response.");
         }
-
 
         //抛出错误信息
         if (isset($response->error)) {
@@ -114,7 +112,7 @@ class RpcClient
         //拼接字符成字符串
         $aliParams = [];
         foreach ($protocol as $key => $value) {
-            $value = is_array($value) ? json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) : '';
+            $value = is_array($value) ? json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) : $value;
             $aliParams[] = $key . $value;
         }
         $signStr = $this->signKey . implode('', $aliParams);
@@ -124,6 +122,28 @@ class RpcClient
     }
 
 
+    /**
+     * 参数url编码
+     * @param $params
+     * @return mixed
+     */
+    private function urlencodeParams($params)
+    {
+        foreach ($params as $key=>&$val) {
+            urlencode($val);
+        }
+        return $params;
+    }
+
+
+    /**
+     * get请求
+     * @param $url
+     * @param $data
+     * @param $header
+     * @return mixed
+     * @throws Exception
+     */
     private function get($url, $data, $header)
     {
         $log = $this->log;
@@ -134,11 +154,25 @@ class RpcClient
         //头部设置    
         $header = !empty($header) ? $header : array("Content-type: x-www-form-urlencoded");
 
+        //url拼接参数, 生成urlencode之后的请求字符串 (请求参数中有中文时，中文需要经过url编码)
+        $url = $url. '?' . http_build_query($data);
+
+        //发起curl请求
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POST, 0);
+
+        //设置头部信息
+        if (!empty($header)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
+
+        //设置获取的信息以文件流的形式返回，而不是直接输出
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        //设置头文件的信息,不作为数据流输出
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        //设置cURL允许执行的最长秒数(请求超时时间)
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 
         $response = curl_exec($ch);
@@ -162,12 +196,23 @@ class RpcClient
     }
 
 
+    /**
+     * post请求
+     * @param $url
+     * @param $data
+     * @param $header
+     * @return mixed
+     * @throws Exception
+     */
     private function post($url, $data, $header)
     {
         $log = $this->log;
         if ($log != null) {
             $log->info("request data: " . json_encode($data));
         }
+
+        //请求参数中有中文时，中文需要经过url编码
+        $data = $this->urlencodeParams($data);
 
         //头部设置    
         $header = !empty($header) ? $header : array("Content-type: x-www-form-urlencoded");
